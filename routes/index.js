1e25cea6162a6ab5e69de3dff4627b35c565949d
rishirajrandive <rishirajdigambar.randive@sjsu.edu>
2017-05-31 02:09:58 -0700

    Complete the structure for project, add data store, and email

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var twilioService = require('../twilio-service');
 var mandrill = require('node-mandrill')('6gzbwndAhPHpAbMab6aKrg');
 var nodemailer = require('nodemailer');
+var fs = require('fs');
 
 var router = express.Router();
 
@@ -17,55 +18,68 @@ router.post('/sendsms', function (req, res) {
   twilioService.sendSMS(mobileNumber, 'Hellow', function (err) {
     if(err){
       console.log('Sending sms failed');
+      res.send({status_code: 400, response: 'Sending SMS failed '+ err});
     }
     console.log('Show success message');
+    res.send({status_code: 200, response: 'Sending SMS complete '});
   });
 });
 
 router.post('/sendemail', function(req, res) {
-  // console.log('Sending email');
-  // var emailId = req.body.contact;
-  // var message = "Hello";
-  //
-  // mandrill('/messages/send', {
-  //   message: {
-  //     to: [{email: emailId, name: 'Test Name'}],
-  //     from_email: 'randive.rishiraj@gmail.com',
-  //     subject: "Hey, what's up?",
-  //     text: "Hello, I sent this message using mandrill."
-  //   }
-  // }, function(error, response)
-  // {
-  //   //uh oh, there was an error
-  //   if (error) console.log( JSON.stringify(error) );
-  //
-  //   //everything's good, lets see what mandrill said
-  //   else console.log(response);
-  // });
-
-
+  console.log('Sending email');
+  var emailId = req.body.contact;
+  var message = "Hello";
 
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'randive.rishiraj@gmail.com',
-      pass: '20Ty$0n12'
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
     }
   });
 
   var mailOptions = {
-    from: 'randive.rishiraj@gmail.com',
+    from: 'share.messages1@gmail.com',
     to: 'iwtdsn@gmail.com',
     subject: 'Sending Email using Node.js',
     text: 'That was easy!'
   };
 
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
+  transporter.sendMail(mailOptions, function(err, info){
+    if (err) {
+      console.log(err);
+      res.send({status_code: 400, response: 'Sending email failed '+ err})
     } else {
       console.log('Email sent: ' + info.response);
+      res.send({status_code: 200, response: info.response})
     }
   });
 });
+
+exports.saveMessage = function (message, callback) {
+
+  fs.readFile('./message_store.json', 'utf-8', function(err, data) {
+    if (err){
+      console.log('Error reading the file' + err);
+      callback(true);
+    }
+    var arrayOfMessages = JSON.parse(data);
+    var id = Math.floor(Math.random() * (1000 - 1 + 1) + 1);
+    arrayOfMessages.messages.push({
+      message: message,
+      id: id
+    });
+
+    console.log(arrayOfMessages);
+
+    fs.writeFile('./message_store.json', JSON.stringify(arrayOfMessages), 'utf-8', function(err) {
+      if (err){
+        console.log('Error writing the file' + err);
+        callback(true);
+      }
+      console.log('Done!')
+    })
+  })
+};
+
 module.exports = router;
--- routes/show.js
+++ routes/show.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET show listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('show', { title: 'Post Message' });
 });
 
 module.exports = router;
